1fc1e1bf020fdee7a767202e2b74bea824e119c6
Jau Italo <jau.batista@dcx.ufpb.br>
2026-04-01 19:15:50 -0300

    implementado config

---
+++ promocoes/src/main/java/com/seuprojeto/promocoes/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.seuprojeto.promocoes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated()
+        )
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(WithDefaults.jwt()));
+
+    return http.build();
+    }
+    
+}
