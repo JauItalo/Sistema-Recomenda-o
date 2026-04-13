9388568a5850c8d04929eb760e883977482a2ae4
Jau Italo <jau.batista@dcx.ufpb.br>
2026-04-13 18:28:05 -0300

    cadastro implementado, vamos trabalhar em validar ele

--- promocoes-frontend/src/pages/Cadastro.jsx
+++ promocoes-frontend/src/pages/Cadastro.jsx
@@ -1,8 +1,56 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Cadastro = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [erro, setErro] = useState('');
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            await register(name, email, password);
+            navigate('/login');
+        } catch (err) {
+            setErro('Erro ao tentar cadastrar. Verifique os dados e tente novamente.');
+        }
+    };
+
+
+
     return (
         <div>
             <h2>Cadastro</h2>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    placeholder="Nome"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    required
+                />
+                <input 
+                    type="email"
+                    placeholder="E-mail"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                />
+                <input 
+                    type="password"
+                    placeholder="Senha"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                />
+
+                <button type="submit">Cadastrar</button>
+            </form>
+            {erro && <p style={{color: 'red'}}>{erro}</p>}
         </div>
     );
 };
